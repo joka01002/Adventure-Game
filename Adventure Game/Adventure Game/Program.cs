7bcac1b636a8156342caa1a408dc67d7258c210d
joka01002 <joka01002@utb.vaxjo.se>
2019-10-14 10:14:54 +0200

    Added things to use and take statements. Can't go north statement.

--- Adventure Game/Adventure Game/Program.cs
+++ Adventure Game/Adventure Game/Program.cs
@@ -11,6 +11,7 @@ namespace Adventure_Game
         static void Main(string[] args)
         {
             bool play = true;
+            bool hiddenroom = false;
             List<string> items = new List<string>();
             string location = "kitchen";
             Console.WriteLine("Welcome to this adventure game");
@@ -23,18 +24,19 @@ namespace Adventure_Game
             
             while(play)
             {
-                string choice = Console.ReadLine().ToLower();
-                if(choice == "help")
+                string command = Console.ReadLine().ToLower();
+                string[] choice = command.Split(' ');
+                if(choice[0] == "help")
                 {
                     Console.WriteLine("To move write 'go' followed by direction, 'north', 'south', 'east' or 'west'.");
                     Console.WriteLine("To view inventory write 'inventory'.");
-                    Console.WriteLine("To quit the game write 'quit'.");
+                    Console.WriteLine("To quit the game write 'quit' or 'end'.");
                     Console.WriteLine("To look around the place write 'search'.");
                     Console.WriteLine("To search specific places write 'search' followed by location.");
-                    Console.WriteLine("To pick up items write 'pick up' followed by the item.");
+                    Console.WriteLine("To pick up items write 'take' followed by the item.");
                     Console.WriteLine("To use an item in inventory write 'use' followed by the item.");
                 }
-                else if(choice == "inventory")
+                else if(choice[0] == "inventory")
                 {
                     Console.WriteLine("These are the following items that you currently own.");
                     foreach(string i in items)
@@ -42,25 +44,106 @@ namespace Adventure_Game
                         Console.WriteLine(i);
                     }
                 }
-                else if(choice == "quit")
+                else if(choice[0] == "quit" || choice[0] == "end")
                 {
                     play = false;
                 }
-                else if (choice == "go north")
+                else if (choice[0] == "go")
                 {
+                    if (choice[1] == "north")
+                    {
+                        if (location == "kitchen" || location == "pantry" || location == "proch" || location == "washroom")
+                        {
+                            Console.WriteLine("You can't move north in this location.");
+                        }
+                        if (location == "")
+                        {
 
+                        }
+                    }
+                    else if (choice[1] == "south")
+                    {
+
+                    }
+                    else if (choice[1] == "east")
+                    {
+
+                    }
+                    else if (choice[1] == "west")
+                    {
+
+                    }
                 }
-                else if (choice == "go south")
+                else if (choice[0] == "use")
                 {
-
+                    if (location == "doghouse")
+                    {
+                        if (choice[1] == "knife")
+                        {
+                            Console.WriteLine("You need to pass through and stab the knife right into its chest." +
+                                "The dog writhes in pain until it finally stops moving." +
+                                "You can now move forward but with a bad feeling in your heart");
+                        }
+                        if (choice[1] == "rusty" && choice[2] == "knife")
+                        {
+                            Console.WriteLine("You need to pass through and stab the knife right into its chest." +
+                                "The dog writhes in pain until it finally stops moving." +
+                                "You can now move forward but with a bad feeling in your heart");
+                        }
+                        if (choice[1] == "meat")
+                        {
+                            Console.WriteLine("You reach out the meat towards the dog." +
+                                "It takes a while but the dog takes the meat and devours it." +
+                                "The dog seems happy and as you move you see that it follows you." +
+                                "You've gained a companion and of corse it needs a name." +
+                                "What is it's name?");
+                            string name = Console.ReadLine();
+                            items.Add(name + "the Dog");
+                            items.Remove("meat");
+                        }
+                    }
+                    if (location == "washroom")
+                    {
+                        if (choice[1] == "flour")
+                        {
+                            Console.WriteLine("");
+                            hiddenroom = true;
+                        }
+                    }
                 }
-                else if (choice == "go east")
+                else if (choice[0] == "take")
                 {
-
-                }
-                else if (choice == "go west")
-                {
-
+                    if (location == "kitchen")
+                    {
+                        if (choice[1] == "knife")
+                        {
+                            Console.WriteLine("You pick up the rusty knife, " +
+                                "it's not that sharp but could probably kill something if needed");
+                            items.Add("Rusty Knife");
+                        }
+                        if (choice[1] == "rusty" && choice[2] == "knife")
+                        {
+                            Console.WriteLine("You pick up the rusty knife, " +
+                                "it's not that sharp but could probably kill something if needed");
+                            items.Add("Rusty Knife");
+                        }
+                        if (choice[1] == "meat")
+                        {
+                            Console.WriteLine("This meat does not look appetizing" +
+                                "You definitely don't want to eat it but something else might");
+                            items.Add("Rotten meat");
+                        }  
+                    }
+                    if (location == "pantry")
+                    {
+                        if (choice[1] == "flour")
+                        {
+                            Console.WriteLine("You take the flour." +
+                                "When thrown it might make it difficult to see. " +
+                                "You never know what will happen");
+                            items.Add("Flour");
+                        }
+                    }
                 }
                 else
                 {
